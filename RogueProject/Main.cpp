b6e5bf528e43900bec4e0a5d0136e8733ac841a0
Jack <a00241893@student.ait.ie>
2018-09-23 22:10:02 +0100

    items added, can add to inventory, pick up and delete selected items from inventory.

--- RogueProject/Main.cpp
+++ RogueProject/Main.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include <iostream>
 #include <string>
+#include <vector>
 
 const int LEVELWIDTH = 20;
 const int LEVELHEIGHT = 10;
@@ -14,17 +15,17 @@ unsigned int health = 0;
 
 char playerChar = 'P';
 bool invActive = false;
-
+std::vector<std::string> inventory;
 
 char map[LEVELHEIGHT][LEVELWIDTH + 1] =
 { "aaaaaaaaaaaaaaaaaaaa",
 "a         +        a",
+"a  w               a",
 "a                  a",
 "a                  a",
+"a  o            +  a",
 "a                  a",
-"a               +  a",
-"a                  a",
-"a     +            a",
+"a     +         x  a",
 "a          +       a",
 "aaaaaaaaaaaaaaaaaaaa"
 };
@@ -59,6 +60,27 @@ void handleCollisions()
 	{
 		// Increase our health
 		health++;
+
+		// Remove it from the map
+		map[newPlayerPositionY][newPlayerPositionX] = ' ';
+	}
+	if (nextLocation == 'w')
+	{
+		inventory.push_back("weapon");
+
+		// Remove it from the map
+		map[newPlayerPositionY][newPlayerPositionX] = ' ';
+	}
+	if (nextLocation == 'x')
+	{
+		inventory.push_back("armour");
+
+		// Remove it from the map
+		map[newPlayerPositionY][newPlayerPositionX] = ' ';
+	}
+	if (nextLocation == 'o')
+	{
+		inventory.push_back("potion");
 
 		// Remove it from the map
 		map[newPlayerPositionY][newPlayerPositionX] = ' ';
@@ -107,10 +129,24 @@ void handleInput()
 
 void invInput()
 {
-	if (GetAsyncKeyState('G') & 0x8000) //open Game
+	char input;
+	std::cin >> input;
+	if (input == 'g' || input == 'G') //open Game
 	{
 		renderMap();
 		invActive = false;
+	}
+	if (input == 'd' || input == 'D')
+	{
+		int x;
+		gotoXY(0, 20);
+		std::cout << "select the number you want to delete:" << std::endl;
+		std::cin >> x;
+		if (x >= 0 && x <= inventory.size())
+		{
+			inventory.erase(inventory.begin() + x);
+			renderInventory();
+		}
 	}
 }
 
@@ -156,11 +192,16 @@ void renderInventory()
 	clearScene();
 	std::cout << "Inventory:" << std::endl;
 	std::cout << "--------------------" << std::endl;
-
+	for (int i = 0; i < inventory.size(); i++)
+	{
+		std::cout << i << ": " << inventory[i] << std::endl;
+	}
 	std::cout << "--------------------" << std::endl;
 
 	gotoXY(0, 18);
 	std::cout << "Press G to go back to game" << std::endl;
+	gotoXY(0, 19);
+	std::cout << "Press D to delete an item" << std::endl;
 }
 
 void main()
